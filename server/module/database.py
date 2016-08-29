c53cd85150a927119c9c6bb9ad4d68515727ac58
garicchi <xgaricchi@gmail.com>
2016-08-29 19:14:45 +0900

    add mysql connector

--- server/module/database.py
+++ server/module/database.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
-## pip install PyMySQL
+## pip install mysqlclient
 
-
+import MySQLdb
+import json
+import os
 
 if __name__ == '__main__':
+    keypath = os.path.join(os.path.dirname(__file__) ,'../../key.json')
+    with open(keypath,'r') as f:
+        json_obj = json.load(f)
 
+    connector = MySQLdb.connect(
+        user='root',
+        passwd=json_obj['sql-pass'],
+        host=json_obj['ec2-host'],
+        db='ApparelStragegy'
+        )
+
+    cursor = connector.cursor()
+    cursor.execute("select * from clothes")
+    for row in cursor.fetchall():
+        print("ID:" + str(row[0]) + "  NAME:" + row[1])
+    cursor.close()
